0401e5ccfe7a6bfe8fd5b53c48a64f0c97097e3c
warren clark <wclarkcito@gmail.com>
2021-05-10 19:47:51 -0700

    added assets folder to src with images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import contractor from '../assets/contractor';
+import lightbulb from '../assets/lightb';
+import Roster from '../assets/Roster';
+import Weather from '../assets/Weather';
+import employee from '../assets/employee';
+import ecommerce from '../assets/ecommerce';
 class Carousel extends React.Component {
 
     constructor(props) {
@@ -10,47 +15,47 @@ class Carousel extends React.Component {
                     id: 0,
                     title: 'Projectimator',
                     subTitle: 'Find a Contractor the easy way!',
-                    imgSrc: '',
+                    imgSrc: contractor,
                     link: 'https://github.com/wclarkcito/contractor',
                     selected: false
                 },
                 {
-                    id: 0,
+                    id: 1,
                     title: 'Cryptelligent',
                     subTitle: 'Your Cryptocurrency journey begins here',
-                    imgSrc: '',
+                    imgSrc: lightbulb,
                     link: 'https://eroux13.github.io/Cryptelligent',
                     selected: false
                 },
                 {
-                    id: 0,
+                    id: 2,
                     title: 'Weather-Dashboard',
                     subTitle: 'Check your weather forecast here',
-                    imgSrc: '',
+                    imgSrc: Weather,
                     link: 'file:///Users/warrenclark/Weather-Dashboard/index.html',
                     selected: false
                 },
                 {
-                    id: 0,
+                    id: 3,
                     title: 'Employee-Tracker',
                     subTitle: 'Keep track of your employees!',
-                    imgSrc: '',
+                    imgSrc: employee,
                     link: 'https://github.com/wclarkcito/employee-tracker',
                     selected: false
                 },
                 {
-                    id: 0,
+                    id: 4,
                     title: 'Team-Profile-Generator',
                     subTitle: 'Create a roster for your employees',
-                    imgSrc: '',
+                    imgSrc: Roster,
                     link: 'https://github.com/wclarkcito/Team-Profile-Generator',
                     selected: false
                 },
                 {
-                    id: 0,
+                    id: 5,
                     title: 'E-Commerce',
                     subTitle: 'Manage your inventory',
-                    imgSrc: '',
+                    imgSrc: ecommerce,
                     link: 'https://github.com/wclarkcito/e-commerce',
                     selected: false
                 },
