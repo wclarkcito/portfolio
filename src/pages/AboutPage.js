617fef416f3ed81d1a0260ef44dad281e1143b58
warren clark <wclarkcito@gmail.com>
2021-05-27 13:53:28 -0700

    modified about me page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,8 +8,20 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-                <p>
-                    From a very young age I was exposed to a great deal of travel. Having grown up in
+                <p> Full Stack Web Developer with a passion for problem solving. I am proficient with JavaScript, Express, Node.js, MySQL and MongoDb.</p>
+
+                <p>Learned Russian at the age of five and translated in Yalta at the age of 13.</p>
+
+                <p>Graduated with a Bachelor's degree in English from the University of Puget Sound and hold a certificate in Full Stack Web Development from University of California, Davis</p>
+
+                <p>Participated in an Archaeological dig of Tel Jaluul in Amand Jordan and have traveled extensively throughout the world.</p>
+
+                <p>Worked as an Analyst for a Broker/Dealer in Irvine, CA and obtained NASD licenses Series 6, 63 and 3.</p>
+
+                <p>Have spent 15 years in the Service Industry both Managing and revitalizing numerous Bars and Restaurants increasing revenue.</p>
+
+
+                {/* From a very young age I was exposed to a great deal of travel. Having grown up in
                     Southern
                     California it would have been easy to settle into the beach lifestyle; however it was
                     important to my parents that I get out and learn how the rest of the world lived. Thus,
@@ -33,8 +45,8 @@ function AboutPage(props) {
                     Restaurants/Nightclubs. Like many other businesses the hospitality industry plummeted at
                     the
                     hands of Covid-19, so I am now enrolled in a Full-Stack Coding Bootcamp at UC
-                    Davis pursuing a career in Full Stack Web Development.
-                </p>
+                    Davis pursuing a career in Full Stack Web Development. */}
+
             </Content>
         </div>
     )
