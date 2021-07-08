1119e3a72bc2a695bf778685cee5ba0b5281a099
warren clark <wclarkcito@gmail.com>
2021-07-08 14:51:39 -0700

    changed the text in the about page to highlight accomplishments

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,45 +8,25 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-                <p> Full Stack Web Developer with a passion for problem solving. </p>
+                <p> Full Stack Web Developer with a passion for problem solving and a strong background in Sales, Finance and Customer Service. </p>
 
-                <p> Learned Russian at the age of five and worked as a translator for the Santa Barbara Yalta Sister cities organization in 1991.
+                <p> Recently earned a certificate in full stack web development from UC Davis with skills in JavaScript, React.js, Node.js, JQuery, CSS3, HTML5, MySql, MongoDb and Express.
                 </p>
 
-                <p> Graduated with a Bachelor's degree in English from the University of Puget Sound and hold a certificate in Full Stack Web Development from University of California, Davis.</p>
+                <p> Staunch adherence to the Scrum framework.</p>
 
-                <p> Participated in Archaeological digs of Tel Jaluul in Amand Jordan and have traveled extensively throughout the world.</p>
+                <p> Excellent communication skills. </p>
 
-                <p> Worked as an Analyst for Winchester Hedge Fund of Funds in Irvine, CA and obtained NASD licenses Series 6, 63 and 3.</p>
+                <p> Graduated with a Bachelor's degree in English from the University of Puget Sound.</p>
 
-                <p> Have spent 15 years in the Service industry both managing and revitalizing numerous Bars and Restaurants, thus increasing revenue.</p>
+                <p> Former Analyst for Winchester Hedge Fund of Funds in Irvine, CA and obtained NASD licenses Series 6, 63 and 3.</p>
+
+                <p> Have spent 15 years in the Service industry both managing and revitalizing numerous Bars and Restaurants increasing revenue in all respective establishments.</p>
 
 
-                {/* From a very young age I was exposed to a great deal of travel. Having grown up in
-                    Southern
-                    California it would have been easy to settle into the beach lifestyle; however it was
-                    important to my parents that I get out and learn how the rest of the world lived. Thus,
-                    by
-                    the age of eighteen I had traveled in and out of several countries most of which would
-                    be
-                    classified as second or third world. Having also learned Russian at a very young age I
-                    spent time in Yalta and Moscow during the early nineties translating at the latter
-                    stages of
-                    the Cold War. Shortly after High School I spent six months in Ammand, Jordan on an
-                    archeological dig
-                    before attending Oxford University for a year to study Literature. After finishing up
-                    College in the states I relocated to Los Angeles to pursue a career in acting. It did
-                    not
-                    take me long to realize that I was not going to take Hollywood by storm so I began
-                    working
-                    in Private Equity and Investment Banking as an Analyst for a small Broker/Dealer. Once
-                    the
-                    Recession in 2009 descended upon us the financial landscape changed with regards to my
-                    employers so I relocated to Santa Barbara and have spent the last ten years managing
-                    Restaurants/Nightclubs. Like many other businesses the hospitality industry plummeted at
-                    the
-                    hands of Covid-19, so I am now enrolled in a Full-Stack Coding Bootcamp at UC
-                    Davis pursuing a career in Full Stack Web Development. */}
+
+
+
 
             </Content>
         </div>
